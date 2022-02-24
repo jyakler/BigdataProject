fcbb10b152267825eada682312a22c9a52b310af
jyakler <jyakler@gmail.com>
2022-02-24 10:33:41 +0900

    기본적인 틀 만들기

---
+++ bookL/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BooklConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bookL'
--- project1/settings.py
+++ project1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookL',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ko-kr'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    BASE_DIR/'staticfiles',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
