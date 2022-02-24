db702e9335c9d264e7484f6a821bff46be82d515
jyakler <jyakler@gmail.com>
2022-02-24 14:06:41 +0900

    bookL/views작성 -세부적인 html 연결등은 수정필요

--- bookL/urls.py
+++ bookL/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name="main"
 urlpatterns=[
-
+    path('',views.index,name='index'),#메인페이지
+    path('login/',views.login,name='login'),#로그인
+    path('logout/',views.logout,name='logout'),#로그아웃
+    path('register/',views.register,name='register'),#회원가입
 ]
--- bookL/views.py
+++ bookL/views.py
@@ -3,16 +3,71 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 
 #메인페이지
-
+def index(request):
+    context=None
+    if request.user.is_authenticated:
+        context={'logineduser':request.user}
+    return render(request,'메인페이지',context)
+'''
+메인페이지에 로그인했는지 여부를 나누어 메인페이지 상단탭에 로그인/회원가입<->로그아웃 표시 나오게
+'''
 
 #로그인
+def login(request):
+    if request.method=="POST":#로그인 요청이 들어옴
+        useremail=request.POST.get('useremail',None)#사용자 아이디
+        password=request.POST.get('password',None)
+        user=auth.authenticate(username=useremail,password=password)
+    #로그인 성공
+        if user is not None:
+            auth.login(request,user)
+            return redirect('main:index')
+        else:#로그인 실패
+            return render(request,'로그인페이지',{'error':'사용자 아이디 또는 패스워드가 틀립니다.'})
+    else:
+        return render(request,'로그인페이지')
 
 #로그아웃
+def logout(request):
+    if request.user.is_authenticated:
+        auth.logout(request)
+    return redirect('main:index')
 
 #회원가입
 
-#마이페이지
+'''
+account 테이블의 
+username=유저아이디(이메일)
+last_name= 유저닉네임
+password=비번
+'''
+def register(request):
+    res_data=None
+    if request.method == "POST":
+        useremail=request.POST.get('useremail')
+        password=request.POST.get('password')
+        re_password=request.POST.get('re-password')
+        nickname=request.POST.get('nickname')
+        res_data={}
+        if User.objects.filter(username=useremail):
+            res_data['error']='이미 가입된 아이디(이메일)입니다.'
+        elif User.objects.filter(last_name=nickname):
+            res_data['error']='이미 존재하는 닉네임 입니다.'
+        elif password!=re_password:
+            res_data['error']='비밀번호가 다릅니다.'
+        else:
+            user=User.objects.create_user(username=useremail,last_name=nickname,
+                                          password=password)
+            auth.login(request,user)
+            return redirect('main:index')
+    return render(request,'회원가입',res_data)
 
+#마이페이지
+def mypage(request):
+    context=None
+    if request.user.is_authenticated:
+        context={'user':request.user}
+    return render(request,'마이페이지',context)
 #html- 메인페이지, 회원가입페이지, 로그인페이지, 마이페이지
 
 #------------------------------------------
